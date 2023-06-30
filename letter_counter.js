2a7496b815a95918e654fe337f439add0af89113
Kiggundu Brevin <74289360+brevins2@users.noreply.github.com>
2023-06-30 17:16:31 +0300

    Add files via upload
    final commit

--- letter_counter.js
+++ letter_counter.js
@@ -1,25 +1,48 @@
 function letter_counter() {
     para = document.querySelector('input').value
     paragrapgh_in_lowercase =para.toLowerCase()
-    array_of_letters(paragrapgh_in_lowercase);
+    // array_of_letters(paragrapgh_in_lowercase);
+    count(paragrapgh_in_lowercase)
 }
 
 function array_of_letters(sentence) {
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
     heading = 'Letter Index Appearance ';
     console.log(heading);
 
-    let count = 0;
-    let tr_letter = document.getElementById('data').textContent
-    let tr_appear = document.getElementById('appear').textContent
-    let array_string = sentence.split(' ')
-    
-    for (var l = 0; l < array_string.length; l++) {
-        for (var t = 0; t < array_string[l].length; t++) {
-            if (array_string[l][t] == array_string[l][t]) {
-                tr_letter = array_string[l][t]
-                tr_appear = letters.indexOf(array_string[l][t])
+    let array_string = sentence.split('').map(result_string => {
+        return result_string
+    });
+
+    let count = 0
+
+    for(var i = 0; i < array_string.length; i++) {
+        document.querySelector('h5').textContent += array_string[i] + ', '
+        let prev = array_string[i]
+        for(var j = 0; j <= letters.length; j++) {
+            if(array_string[i] == letters[j]) {
+                count += 1
+                let az = document.getElementById('appear').textContent += letters[j]
+                document.getElementById('letter').innerHTML += i +' -> ' + array_string[i] + '->' + count + '<br/>'
+            }
+        }        
+    }
+
+    document.querySelector('input').value = ''
+    document.querySelector('h4').textContent += '' + letters
+}
+
+function count(parameter) {
+    let appearance = 0
+    for(var i = 0; i < parameter.length; i++) {
+        appearance = 1
+        for(let j = 0; j < parameter.length; i++) {
+            console.log(parameter[i] + ' : ' + parameter[j])
+            if(parameter[i] == parameter[j]) {
+                console.log(parameter[i] + ' : ' + parameter[j])
+                console.log("same" )
+                appearance +=  1
             }
         }
     }
